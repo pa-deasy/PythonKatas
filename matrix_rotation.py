6f23186419b5dc2c467444d6bedcd3188e18fa56
patrick.deasy <pa-deasy@hotmail.com>
2023-03-04 11:52:15 -0800

    Rotate matrix 90 anti clockwise

--- matrix_rotation.py
+++ matrix_rotation.py
@@ -6,4 +6,12 @@ class Matrix:
     cells: list[list[int]]
 
     def rotate_90_anti_clockwise(self) -> None:
-        print()
+        rotated_cells: list[list[int]] = []
+        
+        for column_index in range(len(self.cells) - 1, -1, -1):
+            rotated_row: list[int] = []
+            for row_index in range(0, len(self.cells)):
+                rotated_row.append(self.cells[row_index][column_index])
+            rotated_cells.append(rotated_row)
+            
+        self.cells = rotated_cells
