7b260b3e37d0587ba6ab613dda4fbf260ef9fe5d
patrick.deasy <pa-deasy@hotmail.com>
2023-10-10 08:36:41 -0700

    Linked lists from tree solution

--- tests/trees_and_graphs/test_trees_and_graphs_examples.py
+++ tests/trees_and_graphs/test_trees_and_graphs_examples.py
@@ -1,4 +1,4 @@
-from trees_and_graphs.trees_and_graphs_examples import Node, check_route_exists
+from trees_and_graphs.trees_and_graphs_examples import Node, TreeNode, check_route_exists, create_bst_from_sorted_list, create_linked_lists_from_tree
 
 
 def test_check_route_exists_when_route_exists_then_returns_true():
@@ -33,3 +33,61 @@ def test_check_route_exists_when_route_does_not_exist_then_returns_false():
     does_exist = check_route_exists(node_1, node_7)
     
     assert does_exist is False
+    
+    
+def test_create_bst_from_sorted_list_when_created_then_bst_ordered_as_expected():
+    sorted_numbers = [1, 3, 7, 8, 12, 18, 55, 70, 80]
+    
+    bst = create_bst_from_sorted_list(sorted_numbers)
+    
+    assert bst.value == 12
+    assert bst.left.value == 7
+    assert bst.left.left.value == 3
+    assert bst.left.left.left.value == 1
+    assert bst.left.right.value == 8
+    assert bst.right.value == 70
+    assert bst.right.left.value == 55
+    assert bst.right.left.left.value == 18
+    assert bst.right.right.value == 80
+         
+#                       12
+#               7                   70       
+#           3       8           55      80
+#       1                   18
+
+def test_create_linked_lists_from_tree_when_created_then_lists_as_expected():
+    root_12 = TreeNode(value=12, left=None, right=None)
+    node_7 = TreeNode(value=7, left=None, right=None)
+    node_3 = TreeNode(value=3, left=None, right=None)
+    node_1 = TreeNode(value=1, left=None, right=None)
+    node_8 = TreeNode(value=8, left=None, right=None)
+    node_70 = TreeNode(value=70, left=None, right=None)
+    node_55 = TreeNode(value=55, left=None, right=None)
+    node_18 = TreeNode(value=18, left=None, right=None)
+    node_80 = TreeNode(value=80, left=None, right=None)
+    
+    root_12.left = node_7
+    root_12.left.left = node_3
+    root_12.left.left.left = node_1
+    root_12.left.right = node_8
+    root_12.right = node_70
+    root_12.right.left = node_55
+    root_12.right.left.left = node_18
+    root_12.right.right = node_80
+    
+    linked_lists = create_linked_lists_from_tree(root_12)
+    
+    assert len(linked_lists) == 4
+    assert linked_lists[0].head.value == 12
+    assert linked_lists[0].head.next is None
+    assert linked_lists[1].head.value == 70
+    assert linked_lists[1].head.next.value == 7
+    assert linked_lists[1].head.next.next is None
+    assert linked_lists[2].head.value == 80
+    assert linked_lists[2].head.next.value == 55
+    assert linked_lists[2].head.next.next.value == 8
+    assert linked_lists[2].head.next.next.next.value == 3
+    assert linked_lists[2].head.next.next.next.next is None
+    assert linked_lists[3].head.value == 18
+    assert linked_lists[3].head.next.value == 1
+    assert linked_lists[3].head.next.next is None
--- trees_and_graphs/trees_and_graphs_examples.py
+++ trees_and_graphs/trees_and_graphs_examples.py
@@ -1,19 +1,24 @@
 from dataclasses import dataclass
+import math
+from queue import Queue
+from typing import List
+
+from linked_lists.linked_lists_examples import LinkedList, Node
 
 
 # 4.1 - Route Between Nodes: Given a directed graph and two nodes (S and E), design an algorithm to find out whether there is a route from S to E.
 @dataclass
-class Node:
+class GraphNode:
     name: str
-    children: list['Node']
+    children: list['GraphNode']
     visited: bool = False
 
 
-def check_route_exists(start: Node, end: Node) -> bool:
+def check_route_exists(start: GraphNode, end: GraphNode) -> bool:
     return _check_target_exists(start, end.name)
 
 
-def _check_target_exists(current: Node, target: str) -> bool:
+def _check_target_exists(current: GraphNode, target: str) -> bool:
     if not current or current.visited:
         return False
     
@@ -30,3 +35,49 @@ def _check_target_exists(current: Node, target: str) -> bool:
     return exists
 
 
+# 4.2 - Minimal Tree: Given a sorted(increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+@dataclass
+class TreeNode:
+    value: int
+    left: 'TreeNode'
+    right: 'TreeNode'
+    
+
+def create_bst_from_sorted_list(numbers: list[int]) -> TreeNode:
+    if not numbers:
+        return None
+    
+    mid = math.floor(len(numbers) / 2)
+    left = create_bst_from_sorted_list(numbers[:mid])
+    right = create_bst_from_sorted_list(numbers[mid+1:])
+    node = TreeNode(value=numbers[mid], left=left, right=right)
+    
+    return node
+
+
+# 4.3 - List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth
+# (e.g. if you have a tree with depth D, you'll have D linked lists).
+def create_linked_lists_from_tree(root: TreeNode) -> List[LinkedList]:
+    linked_lists: List[LinkedList] = []
+    queue = [root]
+    
+    while queue:
+        next_queue = []
+        linked_list = LinkedList(head=None)
+        
+        while queue:
+            dequeued = queue.pop(0)
+            node = Node(value=dequeued.value, next=None)
+            node.next = linked_list.head
+            linked_list.head = node
+            
+            if dequeued.left:
+                next_queue.append(dequeued.left)
+            if dequeued.right:
+                next_queue.append(dequeued.right)
+        
+        linked_lists.append(linked_list)    
+        queue = next_queue
+        
+    return linked_lists
+            
